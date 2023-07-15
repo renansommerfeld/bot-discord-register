a041f330a09ce0c9912ff8fd1d3e2716e6df9a9f
Renan Sommerfeld <renan@must.pt>
2023-07-15 18:35:27 +0100

    Ajuste no registro

--- commands/registro.js
+++ commands/registro.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, MessageEmbed, User } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -24,8 +26,14 @@ module.exports = {
     const nome = interaction.options.getString('nome');
     const id = interaction.options.getString('id');
     const contratante = interaction.options.getUser('contratante');
-    const tagRoleId = '1128025823425871992'; // ID do cargo da tag
-    const responseChannelId = '1128308534044528652'; // ID do canal para a resposta
+    const tagRoleId = process.env.TAG_ROLE_ID; // ID do cargo da tag
+    const responseChannelId = process.env.RESPONSE_CHANNEL_ID; // ID do canal para a resposta
+    const allowedChannelId = process.env.ALLOWED_CHANNEL_ID; // ID do canal permitido
+
+    if (interaction.channelId !== allowedChannelId) {
+      const allowedChannel = interaction.guild.channels.cache.get(allowedChannelId);
+      return interaction.reply(`Este comando só pode ser executado no canal ${allowedChannel}`).catch(console.error);
+    }
 
     const tagRole = interaction.guild.roles.cache.get(tagRoleId);
 
@@ -38,13 +46,13 @@ module.exports = {
       return interaction.reply('O ID deve conter apenas números.');
     }
 
-    await interaction.member.setNickname(`${id} | ${nome}`);
+    await interaction.member.setNickname(`[M] ${nome} | ${id}`);
 
     // Adicionar a tag
     await interaction.member.roles.add(tagRole);
     console.log(`Tag "${tagRole.name}" adicionada para o usuário ${interaction.member.user.tag}`);
 
-    interaction.reply(`Nome atualizado para ${id} | ${nome}`).then(reply => {
+    interaction.reply(`Nome atualizado para ${nome} | ${id}`).then(reply => {
       setTimeout(() => {
         reply.delete().catch(console.error);
       }, 10000); // 10 segundos (em milissegundos)
@@ -57,6 +65,6 @@ module.exports = {
       return interaction.reply('O ID do canal de resposta é inválido.');
     }
 
-    responseChannel.send(`Usuário ${interaction.member.user.tag} atualizou o nome para ${id} | ${nome}. Contratante: ${contratante}`);
+    responseChannel.send(`Usuário ${interaction.member.user.tag} atualizou o nome para [M] ${nome} | ${id}. Contratante: ${contratante}`);
   },
 };
